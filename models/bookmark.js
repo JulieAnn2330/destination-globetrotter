70d3e1b27fe507c4f441f7e9a235b13d64e27fd0
Julie Schaub <julie.schaub@hotmail.com>
2020-11-27 10:16:57 -0600

    updated bookmark

--- models/bookmark.js
+++ models/bookmark.js
@@ -3,6 +3,13 @@ module.exports = function (sequelize, DataTypes) {
     data: {
       type: DataTypes.TEXT,
       allowNull: false,
+      get: function () {
+        return JSON.parse(this.getDataValue("data"));
+      },
+      set: function (value) {
+        console.log("value from bookmark.js = ", value);
+        this.setDataValue("data", JSON.stringify(value));
+      },
     },
     userId: {
       type: DataTypes.INTEGER,
